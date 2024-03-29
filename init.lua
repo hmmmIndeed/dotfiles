0b7f902ece0a6ff78d311c64242a7247279017ec
hmmmIndeed <73439762+hmmmIndeed@users.noreply.github.com>
2024-03-29 14:03:31 -0400

    nvim

---
+++ init.lua
@@ -0,0 +1,13 @@
+vim.cmd([[
+set number
+set clipboard=unnamedplus
+set cursorline
+set ttyfast
+
+set tabstop=4
+set shiftwidth=4
+set softtabstop=4
+set noexpandtab
+set smartindent
+set smarttab
+]])
